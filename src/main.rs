91102d522678dd7ceef77ca17928567ee2f217af
khoidiangelo <timhorochowik@icloud.com>
2023-05-12 19:20:17 +0200

    new stuff

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,46 @@ fn read_input() -> String {
     input.trim().to_string()
 }
 
-fn convert_to_temp(temperature: i32) {
+fn convert_to_temp() {
     loop {
+        println!("input degrees as number");
+        let temperature: i32 = read_input().parse::<i32>().unwrap_or_default();
         println!("Do you want to convert to celsius or fahrenheit?");
         println!("celsius -> c | fahrenheit -> f");
         let to_temp = read_input();
         if to_temp.trim() == "c" {
             let result = ((temperature - 32) * 5) / 9;
             println!("{result}");
-            drop(result);
-            break;
         } else if to_temp.trim() == "f" {
             let result = ((temperature * 9) / 5) + 32;
             println!("{result}");
-            drop(result);
-            break;
         } else {
             println!("no valid input found");
-            continue;
         }
+        drop(to_temp);
+        drop(temperature);
     }
-    drop(temperature);
+}
+
+fn is_divisible_by_two() {
+    println!("Is divisible by two?");
+    loop {
+        let number: i32 = read_input().parse::<i32>().unwrap_or_default();
+        if number % 2 == 0 {
+            println!("yes");
+        } else {
+            println!("no");
+        }
+        drop(number);
+    }
 }
 
 fn main() {
     println!("Hello, world!");
-    println!("Input your temperature as a number only");
-    convert_to_temp(read_input().parse::<i32>().unwrap_or_default());
+    println!("de | di");
+    match read_input().trim() {
+        "de" => convert_to_temp(),
+        "di" => is_divisible_by_two(),
+        _ => println!("no valid input"),
+    }
 }

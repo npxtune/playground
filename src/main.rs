b39fce2d149cbc63f08450d691f6a3ee5ec58ffe
khoidiangelo <timhorochowik@icloud.com>
2023-05-28 18:59:15 +0200

    changed rustfmt + nightly

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-fn read_input() -> String {
+fn read_input() -> String
+{
     let mut input = String::new();
     std::io::stdin()
         .read_line(&mut input)
@@ -6,44 +7,56 @@ fn read_input() -> String {
     input.trim().to_string()
 }
 
-fn convert_to_temp() {
-    loop {
+fn convert_to_temp()
+{
+    loop
+    {
         println!("input degrees as number");
         let temperature: i32 = read_input().parse::<i32>().unwrap_or_default();
         println!("Do you want to convert to celsius or fahrenheit?");
         println!("celsius -> c | fahrenheit -> f");
         let to_temp = read_input();
-        if to_temp.trim() == "c" {
+        if to_temp.trim() == "c"
+        {
             let result = ((temperature - 32) * 5) / 9;
             println!("{result}");
-        } else if to_temp.trim() == "f" {
+        }
+        else if to_temp.trim() == "f"
+        {
             let result = ((temperature * 9) / 5) + 32;
             println!("{result}");
-        } else {
+        }
+        else
+        {
             println!("no valid input found");
         }
         drop(to_temp);
-        drop(temperature);
     }
 }
 
-fn is_divisible_by_two() {
+fn is_divisible_by_two()
+{
     println!("Is divisible by two?");
-    loop {
+    loop
+    {
         let number: i32 = read_input().parse::<i32>().unwrap_or_default();
-        if number % 2 == 0 {
+        if number % 2 == 0
+        {
             println!("yes");
-        } else {
+        }
+        else
+        {
             println!("no");
         }
-        drop(number);
     }
 }
 
-fn main() {
+fn main()
+{
     println!("Hello, world!");
     println!("de | di");
-    match read_input().trim() {
+    match read_input().trim()
+    {
         "de" => convert_to_temp(),
         "di" => is_divisible_by_two(),
         _ => println!("no valid input"),

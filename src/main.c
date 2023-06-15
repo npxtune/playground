24391df28586667404aed71711f06b8f76167e99
khoidiangelo <horochowiktik@pm.me>
2023-06-15 22:16:21 +0200

    stuff

--- src/main.c
+++ src/main.c
@@ -2,16 +2,10 @@
 
 #include <stdio.h>
 
-#define UPPER 300
-#define LOWER 0
-#define STEP 20
-
- // prints values of Fahrenheit and Celsius in a list with decrements of 20 from 300 -> 0
-
  int main() {
-     printf("%3s\t%6s\n","Fahrenheit", "Celsius");
-     for (int fahr = UPPER; fahr >= LOWER; fahr -= STEP) {
-         printf("%5d\t%14.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
+    int c;
+     while ((c = getchar()) != EOF) {
+         putchar(c);
      }
      return 0;
  }

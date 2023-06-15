1d107daf5df47677431943b963a8f41ec3f921f1
khoidiangelo <horochowiktik@pm.me>
2023-06-15 16:11:17 +0200

    f to c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    printf("Trisi ist toll und OSX > Windows\n");
-    return 0;
-}
+ int main() {
+     double fahr, celsius;
+     int lower = 0, upper = 100, step = 20;
+
+     fahr = lower;
+     printf("%3s\t%6s\n","Fahrenheit", "Celsius");
+     while (fahr <= upper) {
+         celsius = (5.0 / 9.0) * (fahr-32.0);
+         printf("%5.0f\t%14.1f\n", fahr, celsius);
+         fahr = fahr + step;
+     }
+     return 0;
+ }

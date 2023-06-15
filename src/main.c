1a68fc8c7695e457603f03583e313bad71bafebc
khoidiangelo <horochowiktik@pm.me>
2023-06-16 01:06:50 +0200

    use main .gitignore file

--- src/main.c
+++ src/main.c
@@ -2,10 +2,7 @@
 
 #include <stdio.h>
 
- int main() {
-    int c;
-     while ((c = getchar()) != EOF) {
-         putchar(c);
-     }
-     return 0;
- }
+int main() {
+    printf("%d\n", EOF);
+    return 0;
+}

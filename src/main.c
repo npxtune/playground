d4da42382ce1136b43040e8ae3b2cc97ea73865a
khoidiangelo <horochowiktik@pm.me>
2023-06-18 23:30:26 +0200

    k&r 1-14

--- src/main.c
+++ src/main.c
@@ -1,36 +1,45 @@
 // Copyright (c) 2023. Created by user khoidiangelo on GitHub.
 
 #include <stdio.h>
-#define MAXWORDS 256
-#define IN 1
-#define OUT 0
-
 int main() {
-    int c, state = OUT, wlength = 0;
-    int alength[MAXWORDS];
-    for (int i = 0; i < MAXWORDS; ++i) {
-        alength[i] = 0;
+    // 97 -> 122 is a -> c
+    // 65 -> 90 is A -> Z
+    int c, nwhite = 0;
+    int clength[25], dlength[9];
+    for (int i = 0; i < 25; ++i) {
+        clength[i] = 0;
+    }
+    for (int i = 0; i < 9; ++i) {
+        dlength[i] = 0;
     }
     while ((c = getchar()) != EOF) {
-        if (c == ' ' || c == '\t' || c == '\n') {
-            if (state == IN) {
-                state = OUT;
-                ++alength[wlength];
-                wlength = 0;
-            }
+        if (c >= 97 && c <= 122) {
+            ++clength[c -  97];
+        } else if (c >= 65 && c <= 90) {
+            ++clength[c - 65];
+        } else if (c >= '0' && c <= '9') {
+            ++dlength[c - '0'];
         } else {
-            state = IN;
-            ++wlength;
+            ++nwhite;
         }
     }
-    for (int i = 0; i < MAXWORDS; ++i) {
-        if (alength[i] != 0) {
-            printf("%3d: ", i);
-            for (int j = 0; j < alength[i]; ++j) {
-                printf("|");
-            }
-            printf("\n");
+    for (int i = 0; i < 25; ++i) {
+        printf("%4c%c: ", i + 65, i + 97);
+        for (int j = 0; j < clength[i]; ++j) {
+            putchar('|');
         }
+        printf("\n");
+    }
+    for (int i = 0; i < 9; ++i) {
+        printf("%5d: ", i);
+        for (int j = 0; j < dlength[i]; ++j) {
+            putchar('|');
+        }
+        printf("\n");
+    }
+    printf("space: ");
+    for (int i = 0; i < nwhite; ++i) {
+        putchar('|');
     }
     return 0;
 }

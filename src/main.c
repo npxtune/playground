daaa31f9a565a059438f13e455429182f8cefb4a
khoidiangelo <horochowiktik@pm.me>
2023-06-18 21:53:19 +0200

    k&r 1-13

--- src/main.c
+++ src/main.c
@@ -1,22 +1,35 @@
 // Copyright (c) 2023. Created by user khoidiangelo on GitHub.
 
 #include <stdio.h>
+#define MAXWORDS 256
 #define IN 1
 #define OUT 0
 
-// program that prints input one word per line
-
 int main() {
-    int c, state = OUT;
+    int c, state = OUT, wlength = 0;
+    int alength[MAXWORDS];
+    for (int i = 0; i < MAXWORDS; ++i) {
+        alength[i] = 0;
+    }
     while ((c = getchar()) != EOF) {
         if (c == ' ' || c == '\t' || c == '\n') {
             if (state == IN) {
                 state = OUT;
-                putchar('\n');
+                ++alength[wlength];
+                wlength = 0;
             }
         } else {
             state = IN;
-            putchar(c);
+            ++wlength;
+        }
+    }
+    for (int i = 0; i < MAXWORDS; ++i) {
+        if (alength[i] != 0) {
+            printf("%3d: ", i);
+            for (int j = 0; j < alength[i]; ++j) {
+                printf("|");
+            }
+            printf("\n");
         }
     }
     return 0;

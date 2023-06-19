415d93e485d8eeaa9b9052770be2d577cbf5ad55
khoidiangelo <horochowtiktim@pm.me>
2023-06-19 22:55:38 +0200

    k&r: functions chapter

--- src/main.c
+++ src/main.c
@@ -1,46 +1,15 @@
 // Copyright (c) 2023. Created by user khoidiangelo on GitHub.
 #include <stdio.h>
-#define MAXCHAR 26
-#define MAXDIG 10
+int power (int base, int n) {
+    int i;
+    for (i = 1; n > 0; --n) {
+        i = i * base;
+    }
+    return i;
+}
 int main() {
-    // 97 -> 122 is a -> c values of corresponding character in int
-    // 65 -> 90  is A -> Z values of corresponding character in int
-    int c, nother = 0;
-    int clength[MAXCHAR], dlength[MAXDIG];
-    for (int i = 0; i < MAXCHAR; ++i) {
-        clength[i] = 0;
-    }
-    for (int i = 0; i < MAXDIG; ++i) {
-        dlength[i] = 0;
-    }
-    while ((c = getchar()) != EOF) {
-        if (c >= 97 && c <= 122) {
-            ++clength[c -  97];
-        } else if (c >= 65 && c <= 90) {
-            ++clength[c - 65];
-        } else if (c >= '0' && c <= '9') {
-            ++dlength[c - '0'];
-        } else {
-            ++nother;
-        }
-    }
-    for (int i = 0; i < MAXCHAR; ++i) {
-        printf("%4c%c: ", i + 65, i + 97);
-        for (int j = 0; j < clength[i]; ++j) {
-            putchar('|');
-        }
-        printf("\n");
-    }
-    for (int i = 0; i < MAXDIG; ++i) {
-        printf("%5d: ", i);
-        for (int j = 0; j < dlength[i]; ++j) {
-            putchar('|');
-        }
-        printf("\n");
-    }
-    printf("other: ");
-    for (int i = 0; i < nother; ++i) {
-        putchar('|');
+    for (int i = 1; i <= 10; ++i) {
+        printf("%d\n", power(2, i));
     }
     return 0;
 }

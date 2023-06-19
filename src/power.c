937bc7e6249060f74756791dbdee38eb1674fc51
Tim <horochowiktim@pm.me>
2023-06-20 00:16:18 +0200

    yet another test

--- src/power.c
+++ src/power.c
@@ -1,8 +1,4 @@
 // Copyright (c) 2023. Created by user khoidiangelo on GitHub.
-//
-// Created by Tim Horochowik on 20.06.23.
-//
-
 int power (int base, int n) {
     int i;
     for (i = 1; n > 0; --n) {

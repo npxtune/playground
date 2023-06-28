b911e3cc4d6020ca429bbc445038a256210f7a6a
Tim <horochowiktim@pm.me>
2023-06-28 12:43:06 +0200

    K&R 2-1

--- src/main.c
+++ src/main.c
@@ -11,9 +11,14 @@ void print_using_standard(void) {
     printf("signed double: %g  %g\nunsigned double: %g %g\n", DBL_MIN, DBL_MAX, 0.0f, DBL_TRUE_MIN);
 }
 void print_using_computation(void) {
-    // TODO
+    printf("signed char: %d  %d\t\t\t\t\tunsigned char: %u %u\n", -(1 << 7), (1 << 7) -1, 0 , (1 << 8)-1);
+    printf("signed short: %d  %d\t\t\t\tunsigned short: %u %u\n", -(1 << 15), (1 << 15)-1, 0, (1 << 16)-1);
+    printf("signed int: %d  %d\t\t\tunsigned int: %u %u\n", -(1 << 31), (1 << 31)-1, 0, (1u >> 31u)-1u);
+    printf("signed long: %ld  %ld\tunsigned long: %lu %lu\n\n", -(1l << 63l), (1l << 63l)-1l, 0l, (1l >> 31)-1);
+    // Yes I am a maniac I did not use math.c :)
 }
 int main(void) {
+    print_using_computation();
     print_using_standard();
     return 0;
 }

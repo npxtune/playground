2aa39f900658d00488af1b8586cba8177caeecfb
Tim <horochowiktim@pm.me>
2023-07-05 20:29:49 +0200

    K&R C2-7

--- src/main.c
+++ src/main.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
-#include "readline.c"
-#define MAXLINE 1000
-int stringlength(char s[]) {
-    int i = 0;
-    while (s[i] != '\0') {
-        ++i;
-    }
-    return i;
-}
-void is_leap_year(int year) {
-    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-        printf("%d is a leap year.\n", year);
-    } else {
-        printf("%d is not a leap year!\n", year);
-    }
-}
+//#include "readline.c"
+//#define MAXLINE 1000
+//int stringlength(char s[]) {
+//    int i = 0;
+//    while (s[i] != '\0') {
+//        ++i;
+//    }
+//    return i;
+//}
+//void is_leap_year(int year) {
+//    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+//        printf("%d is a leap year.\n", year);
+//    } else {
+//        printf("%d is not a leap year!\n", year);
+//    }
+//}
 int main(void) {
-    char line[MAXLINE];
-    readline(line, MAXLINE);
-    printf("%s", line);
+    if (-1L < 1U) {
+        printf("TRUE\n");
+    }
     return 0;
 }

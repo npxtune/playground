d5612f08ebb234a3318ea4b773c6965856de2940
Tim <horochowiktim@pm.me>
2023-07-11 21:40:50 +0200

    K&R 2-4

--- src/main.c
+++ src/main.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
-//#include "readline.c"
-//#define MAXLINE 1000
-//int stringlength(char s[]) {
-//    int i = 0;
-//    while (s[i] != '\0') {
-//        ++i;
-//    }
-//    return i;
-//}
-//void is_leap_year(int year) {
-//    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-//        printf("%d is a leap year.\n", year);
-//    } else {
-//        printf("%d is not a leap year!\n", year);
-//    }
-//}
-int main(void) {
-    if (-1L < 1U) {
-        printf("TRUE\n");
+#include "readline.c"
+#define MAXLINE 1000
+void squeeze(char s1[], char s2[]) {
+    int i, j, k;
+    for (k = 0; s2[k] != '\0'; ++k) {
+        for (i = j = 0; s1[i] != '\0'; ++i) {
+            if (s1[i] != s2[k]) {
+                s1[j++] = s1[i];
+            }
+        }
     }
+    s1[k] = '\0';
+}
+
+int main(void) {
+    char s1[MAXLINE];
+    readline(s1, MAXLINE);
+    squeeze(s1, "hello");
+    printf("%s", s1);
     return 0;
 }
--- src/readline.c
+++ src/readline.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
-int readline(char s[], int length) {    // rewritten for K&R2-2
+int readline(char s[], int length) {
     int i, c;
-    for (i = 0; i < length-1; ++i) {
-        if ((c = getchar()) != EOF) {
-            if (c != '\n') {
-                s[i] = c;
-            } else {
-                break;
-            }
-        } else {
-            break;
-        }
+    for (i = 0; i < length-1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+        s[i] = c;
     }
     if (c == '\n') {
-        s[i] = c;
-        ++i;
+        s[i++] = c;
     }
     s[i] = '\0';
     return i;

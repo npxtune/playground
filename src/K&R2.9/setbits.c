6e7234f6fc25b60a4303dfbb77bfd237fb91ccbe
Tim <horochowiktim@pm.me>
2023-07-20 20:57:48 +0200

    works now

--- src/K&R2.9/setbits.c
+++ src/K&R2.9/setbits.c
@@ -3,6 +3,6 @@ unsigned int setbits(unsigned int y, unsigned int x, int p, int n) {
     unsigned int y_bits = getbits(y, n-1, n);   // get bits to be replaced
     unsigned bitmask = (~(~0 << n)) << (p-n);   // create mask to reset bits to be replaced
     y_bits = y_bits << (p-n);   // move bits from y to max p position
-    x = (x ^ bitmask) | y_bits;    // set bits in x to be replaced to 0 & replace bits with y
+    x = ((x | bitmask) ^ bitmask) | y_bits;    // set bits in x to be replaced to 0 & replace bits with y
     return x;
 }
--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "K&R2.9/setbits.c"
 int main(void) {
-    printf("%d\n", setbits(0b000101, 0b011001, 6, 3));
-    printf("%d\n", 0b101001); //FOR DEBUGGING
+    printf("%d\n", setbits(0b000101, 0b011001, 4, 3));
+    printf("%d\n", 0b011011); //FOR DEBUGGING
     return 0;
 }

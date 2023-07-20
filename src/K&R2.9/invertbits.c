ade247778d26c1f1c146570250757ebb994b32b7
Tim <horochowiktim@pm.me>
2023-07-20 21:58:12 +0200

    K&R2-7

--- src/K&R2.9/invertbits.c
+++ src/K&R2.9/invertbits.c
@@ -1,3 +1,4 @@
+#include "setbits.c"
 unsigned int invertbits(unsigned int x, int p, int n) {
-    return x;
+    return setbits(getbits(~x, p-1, n), x, p, n);
 }
--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include "K&R2.9/setbits.c"
+#include "K&R2.9/invertbits.c"
 int main(void) {
-    printf("%d\n", setbits(0b000101, 0b011001, 4, 3));
-    printf("%d\n", 0b011011); //FOR DEBUGGING
+    printf("%d\n", invertbits(0b101110, 6, 3));
+    printf("%d\n", 0b010110); //FOR DEBUGGING
     return 0;
 }

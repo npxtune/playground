b38cd526486ba0b043a9ea6ba2d39eab5d75b317
npxtune <timh-development@pm.me>
2023-08-07 18:00:41 +0200

    test

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "K&R2.9/rightrot.c"
 int main(void) {
-    printf("%d\n", 0b110101);
-    printf("%d\n", rightrot(0b101011, 1));
+    printf("%d\n", 0b1010110);
+    printf("%d\n", rightrot(0b1010110, 7));
     printf("\n");
     printf("%d\n", 0b111010);
     printf("%d\n", rightrot(0b101011, 2));

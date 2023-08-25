121b9c909890218bd5d649a7c690cf1879c95add
Tim <horochowiktim@pm.me>
2023-08-25 22:31:25 +0200

    K&R 2-10

--- src/main.c
+++ src/main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include "K&R2.9/rightrot.c"
-#include "K&R2.10/bitcount.c"
+#include "K&R2.11/lower.c"
+#include "readline.c"
+#define MAXLINE 1000
 int main(void) {
-    /*printf("%d\n", 0b1010110);
-    printf("%d\n", rightrot(0b1010110, 7));
-    printf("\n");
-    printf("%d\n", 0b111010);
-    printf("%d\n", rightrot(0b101011, 2));*/
-    printf("%d", bitcount(19));
+    char input[MAXLINE];
+    readline(input, MAXLINE);
+    lower(input);
+    printf("%s", input);
     return 0;
 }

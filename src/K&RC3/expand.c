126e77d9ecbeee4a2120fcb4bb3d270a19344641
Tim <horochowiktim@pm.me>
2023-10-03 23:15:45 +0200

    expand.c now more readable and digit function works

--- src/K&RC3/expand.c
+++ src/K&RC3/expand.c
@@ -1,34 +1,39 @@
 #include <ctype.h>
-#include <stdio.h>
+char new_char = '\0', previous_char = '\0';
+bool found_dash;
+int index2 = 0;
+void expand_character(char s1[], char s2[], int i) {    // CHARACTER FUNCTION
+    if (new_char == '\0') {
+        new_char = s1[i];
+    } else if (found_dash && s1[i] > new_char) {
+        previous_char = s1[i];
+        for (char j = new_char; tolower(j) - '1' != tolower(previous_char) - '0'; ++j, ++index2) {
+            s2[index2] = j;
+        }
+        new_char = previous_char;
+        s2[index2++] = '\n';
+        found_dash = false;
+    }
+}
+void expand_digit(char s1[], char s2[], int i) {    // DIGIT FUNCTION
+    if (new_char == '\0') {
+        new_char = s1[i];
+    } else if (found_dash && s1[i] > new_char) {
+        previous_char = s1[i];
+        for (char j = new_char; j != previous_char + 1; ++j, ++index2) {
+            s2[index2] = j;
+        }
+        new_char = previous_char;
+        s2[index2++] = '\n';
+        found_dash = false;
+    }
+}
 void expand(char s1[], char s2[]) {
-    char new_char = '\0', previous_char = '\0';
-    bool found_dash;
-    int index2 = 0;
     for (int i = 0; s1[i] != '\0'; ++i) {
         if (tolower(s1[i]) >= 'a' && tolower(s1[i] <= 'z')) {       // CHARACTER DETECTION
-            if (new_char == '\0') {
-                new_char = s1[i];
-            } else if (found_dash) {
-                previous_char = s1[i];
-                for (char j = new_char; tolower(j) - '1' != tolower(previous_char) - '0'; ++j, ++index2) {
-                    s2[index2] = j;
-                }
-                new_char = previous_char;
-                s2[index2++] = '\n';
-                found_dash = false;
-            }
+            expand_character(s1, s2, i);
         } else if (s1[i] >= '0' && s1[i] <= '9') {      // DIGIT DETECTION
-            if (new_char == '\0') {
-                new_char = s1[i];
-            } else if (found_dash) {
-                previous_char = s1[i];
-                for (char j = new_char; j != '9' + 1; ++j, ++index2) {
-                    s2[index2] = j;
-                }
-                new_char = previous_char;
-                s2[index2++] = '\n';
-                found_dash = false;
-            }
+            expand_digit(s1, s2, i);
         } else if (s1[i] == '-') {
             found_dash = true;
         }

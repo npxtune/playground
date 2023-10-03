2ddc54f504beca4395535b8ca55508c65ec6fbc1
Tim <horochowiktim@pm.me>
2023-10-03 19:51:04 +0200

    K&R 3.5

--- src/K&RC3/expand.c
+++ src/K&RC3/expand.c
@@ -1,20 +1,36 @@
 #include <ctype.h>
 #include <stdio.h>
 void expand(char s1[], char s2[]) {
-    char first_char = '\0';
+    char new_char = '\0', previous_char;
     bool found_dash;
+    int index2 = 0;
     for (int i = 0; s1[i] != '\0'; ++i) {
-        if (tolower(s1[i]) >= 'a' && tolower(s1[i] <= 'z')) {
-            if (first_char == '\0') {
-                first_char = s1[i];
-            }   else {
-                // TODO IMPLEMENT TILL CHAR
-                first_char = s1[i];
+        if (tolower(s1[i]) >= 'a' && tolower(s1[i] <= 'z')) {       // CHARACTER DETECTION
+            if (new_char == '\0') {
+                new_char = s1[i];
+            } else if (found_dash) {
+                previous_char = s1[i];
+                for (char j = new_char; tolower(j) - '1' != tolower(previous_char) - '0'; ++j, ++index2) {
+                    s2[index2] = j;
+                }
+                new_char = '\0';
+                s2[index2++] = '\n';
+                found_dash = false;
+            }
+        } else if (s1[i] >= '0' && s1[i] <= '9') {      // DIGIT DETECTION
+            if (new_char == '\0') {
+                new_char = s1[i];
+            } else if (found_dash) {
+                previous_char = s1[i];
+                for (char j = new_char; j != '9' + 1; ++j, ++index2) {
+                    s2[index2] = j;
+                }
+                new_char = '\0';
+                s2[index2++] = '\n';
                 found_dash = false;
             }
         } else if (s1[i] == '-') {
             found_dash = true;
-            printf("found dash");
         }
     }
 }
